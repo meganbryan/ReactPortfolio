67c623bb34c72031e281c97c2b88b8119d8ff081
Megan Bryan <meganmbryan12@gmail.com>
2021-04-27 09:04:08 -0700

    Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer class="footer py-4 font-small text-light">
+            <div class="footer-copyright text-center container-fluid">
+                © 2021 Copyright: M Bryan
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
