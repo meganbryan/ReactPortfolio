178d6be927c4ef530ca5a4d6d0231b9aaa8f393d
Megan Bryan <meganmbryan12@gmail.com>
2021-04-27 10:48:38 -0700

    Change structure so the project info is contained within a separate file and displayed using a component

---
+++ src/PortfolioInfo.js
@@ -0,0 +1,51 @@
+import TrackStar from "./assets/groupproject_ss.jpeg";
+import WeatherDashboard_1 from "./assets/SS_1.jpeg";
+import WeatherDashboard_2 from "./assets/SS_2.jpeg";
+import WorkDayScheduler from "./assets/SS_3.jpeg";
+import EmployeeSummaryTemplate from "./assets/SS_7.jpeg";
+import CodeQuiz from "./assets/SS_8.jpeg";
+
+export const projects = [
+    {
+        id: 1,
+        title: "Track Star",
+        description: "All your deliveries, organized in a flash",
+        image: TrackStar,
+        href: "https://track-star-2021.herokuapp.com/"
+    },
+    {
+        id: 2,
+        title: "Weather Dashboard",
+        description: "Should you pack a jacket? Enter your city here.",
+        image: WeatherDashboard_1,
+        href: "https://meganbryan.github.io/WeatherDashboard/"
+    },
+    {
+        id: 3,
+        title: "Should you pack a jacket? Enter your city here.",
+        description: "Weather Dashboard",
+        image: WeatherDashboard_2,
+        href: "https://meganbryan.github.io/WeatherDashboard/"
+    },
+    {
+        id: 4,
+        title: "Work Day Scheduler",
+        description: "Make your nine-to-five scheduling simpler.",
+        image: WorkDayScheduler,
+        href: "https://meganbryan.github.io/WorkDayScheduler/"
+    },
+    {
+        id: 5,
+        title: "Employee Summary Template",
+        description: "Keep track of employee info using this node application.",
+        image: EmployeeSummaryTemplate,
+        href: "https://github.com/meganbryan/EmployeeSummaryTemplate"
+    },
+    {
+        id: 6,
+        title: "Code Quiz",
+        description: "Check your knowledge of chemistry (and my knowledge of coding).",
+        image: CodeQuiz,
+        href: "https://meganbryan.github.io/CodeQuiz/"
+    }
+]
