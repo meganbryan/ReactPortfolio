197d6a2464110c5cafa06719f3b4a12b40cc5db7
Megan Bryan <meganmbryan12@gmail.com>
2021-04-27 09:34:52 -0700

    Change href on name

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -7,7 +7,7 @@ function AboutMe() {
     return (
         <div className = "aboutme">
             <nav className="navbar navbar-expand-lg navbar-dark text-light mb-5 opaque-navbar">
-                <a className="navbar-brand" href="index.html"><h1>Megan Bryan</h1></a>
+                <a className="navbar-brand" href="/"><h1>Megan Bryan</h1></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
