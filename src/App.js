563cff402ce6ee63b120d3b7ead8a8e50606650b
Megan Bryan <meganmbryan12@gmail.com>
2021-04-30 16:05:57 -0700

    Deploy to gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import AboutMe from "./pages/AboutMe";
 import Contact from "./pages/Contact";
